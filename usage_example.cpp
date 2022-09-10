e905beab0230f24424a120da0637b5f08c7b52ea
daumemo <50718225+daumemo@users.noreply.github.com>
2022-09-10 20:36:49 +0300

    Moved generator initialisation outside setup()

--- usage_example.cpp
+++ usage_example.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 #include "ad9833.h"
 
-void setup(){
-  AD9833_IC generator(1, 5, 3); // creates new object with pins 1,5,3 as SCK, FSYNC, DATA
+AD9833_IC generator(1, 5, 3); // creates new object with pins 1,5,3 as SCK, FSYNC, DATA
                                 // and initialises IC with stopped state 
                                 // (reset and sleep states are ON);
-  
+
+void setup(){
   generator.setFrequency(4000, 0); // sets 4kHz frequency in the register 0;
   generator.start(0,0,0); // starts generation with sine wave with 
                           // selected frequency and phase registers (0 and 0).
